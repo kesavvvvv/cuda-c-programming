7784a46af106ae712e1f112680a6f21dc4c6f941
kesavvvvv <kesavr1999@gmail.com>
2025-02-10 14:05:10 -0800

    matrix vector multiplication working

--- matrixVector.cu
+++ matrixVector.cu
@@ -1,3 +1,5 @@
+%%cuda
+
 
 #include <iostream>
 
@@ -7,50 +9,93 @@ void matrixVectorMulKernel(float *b, float *c, float *a, int n) {
     int row = blockDim.x * blockIdx.x + threadIdx.x;
 
     if(row < n) {
-        float sum = 0;
+        float s = 0.0f;
 
         for(int i = 0; i < n; i++) {
-            sum += b[row*n + i] * c[i];
+            s += b[row*n + i] * c[i];
         }
         
-        a[row] = sum;
+        a[row] = s;
     }
 
 }
 
 int main(void) {
-    int n = 1024
+    int n = 100024;
     float *a;
     float *b;
     float *c;
+    float *d;
+
+    cudaSetDevice(0);
+    
+
+    d = (float*)malloc(n*sizeof(float));
 
     a = (float*)malloc(n*n*sizeof(float));
     b = (float*)malloc(n*sizeof(float));
     c = (float*)malloc(n*sizeof(float));
 
+
     for(int i=0; i<n*n; i++) {
-        a[i] = i + 1;
+        a[i] = 1.0;
     }
-    for(i=n*n; i<(n*n + n); i++) {
-        b[i] = i + 1;
+    for(int i=0; i<n; i++) {
+        b[i] = 1.0;
     }
+    /*
+    for(int i=0; i<n; i++) {
+        float s = 0.0f;
+        for(int j=0; j<n; j++) {
+            s += a[i*n + j] * b[j];
+        }
+        d[i] = s;
+    }*/
 
     float *a_d, *b_d, *c_d;
 
-    cudaMalloc(*a_d, n*n*sizeof(float));
-    cudaMalloc(*b_d, n*n*sizeof(float));
-    cudaMalloc(*c_d, n*n*sizeof(float));
+    a_d = (float*)malloc(n*n*sizeof(float));
+    b_d = (float*)malloc(n*sizeof(float));
+    c_d = (float*)malloc(n*sizeof(float));
 
-    cudaMemCpy(a_d, a, n*n*sizeof(float), cudaMemCpyHostToDevice);
-    cudaMemCpy(b_d, b, n*sizeof(float), cudaMemCpyHostToDevice);
+    cudaMalloc(&a_d, n*n*sizeof(float));
+    cudaMalloc(&b_d, n*sizeof(float));
+    cudaMalloc(&c_d, n*sizeof(float));
+
+    cudaMemcpy(a_d, a, n*n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(b_d, b, n*sizeof(float), cudaMemcpyHostToDevice);
     
-    dim3 dimBlock = 256;
-    dim3 dimGrid = ceil(n/256.0);
+    dim3 dimBlock = 1024;
+    dim3 dimGrid = ceil(n/1024.0);
 
-    matrixVectorMulKernel<<<dimBlock, dimGrid>>>(b_d, c_d, a_d, n);
+    std::cout<<ceil(n/256.0);
 
-    cudaMemCpy(c, c_d, n*sizeof(float), cudaMemCpyDeviceToHost);
+    cudaDeviceSynchronize();
+
+    matrixVectorMulKernel<<<dimBlock, dimGrid>>>(a_d, b_d, c_d, n);
+
+
+    cudaMemcpy(c, c_d, n*sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(a_d);
+    cudaFree(b_d);
+    cudaFree(c_d);
+/*
+    for(int i = 0; i<n; i++) {
+        std::cout<<d[i]<<" ";
+    }
+*/
+    int fails = 0;
+    float smallRange = 0.00001;
     
+    for(int i = 0; i < n; i++) {
+        if(c[i] != n) {
+            fails++;
+        }
+        //printf("%f\n", c[i]);
+    }
+    std::cout<<"\n"<<fails<<std::endl;
+    printf("sucess");
+
     return 0;
 }
-
